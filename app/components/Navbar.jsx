122f53881813a4ff722845e6018a856038f2977c
LucasSampaioOliveira <lucas.sampaio33@hotmail.com>
2023-07-17 17:09:36 -0300

    mudando favicon

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -17,10 +17,6 @@ import SearchIcon from '@mui/icons-material/Search';
 const Search = styled('div')(({ theme }) => ({
   position: 'relative',
   borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
   marginLeft: 0,
   width: '100%',
   [theme.breakpoints.up('sm')]: {
